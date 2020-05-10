dc925eaf198b7e4c4a5301a741b0c56ac2ffbd95
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-10 23:44:05 +0530

    Perform String Interpolation

--- DataBinding/src/app/string-interpolation/string-interpolation.component.ts
+++ DataBinding/src/app/string-interpolation/string-interpolation.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./string-interpolation.component.css']
 })
 export class StringInterpolationComponent implements OnInit {
+  
+  Name: String = "Shani Kumar Gupta";
+  UniversityName: String = "GLA University";
+  Course: String = "B.Tech";
+  Branch: String = "Computer Science";
+  UniversityRoll: Number = 171500308;
+  Address = "HH-321 Hindalco Colony Renukoot";
+
 
   constructor() { }
 
