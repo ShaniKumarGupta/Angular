8b3acbaa821c4be02034b75b8e2584b239415688
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-13 14:55:08 +0530

    Add FormsModule and Perform two way data binding on input tag

--- DataBinding/src/app/app.module.ts
+++ DataBinding/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
@@ -18,7 +18,8 @@ import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- DataBinding/src/app/two-way-data-binding/two-way-data-binding.component.ts
+++ DataBinding/src/app/two-way-data-binding/two-way-data-binding.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TwoWayDataBindingComponent implements OnInit {
 
-  uname: string;
+  uname: string="";
 
   constructor() { }
 
